bb9d98e4a8b934d981e711d6846df294a56d8ffb
abdi2148 <abdiqafar.abas@gmail.com>
2020-10-04 20:43:24 +0200

    UnitTest

--- MovieRatingCompulsorySolution/MovieRating.Core/ApplicationService/Implementation/Service.cs
+++ MovieRatingCompulsorySolution/MovieRating.Core/ApplicationService/Implementation/Service.cs
@@ -31,7 +31,7 @@ namespace MovieRating.Core.ApplicationService.Implementation
             }
             if (result.Count == 0)
             {
-                throw new ArgumentException($"No reviews for reviewer with id {reviewer} were found, so an average is not applicable");
+                throw new ArgumentException($"No reviews for reviewer with id {reviewer} were found, ");
             }
             else
             {
@@ -43,7 +43,28 @@ namespace MovieRating.Core.ApplicationService.Implementation
 
         public double GetAverageRateOfMovie(int movie)
         {
-            throw new NotImplementedException();
+            double avg = 0.0;
+            var totalMovieRating = 0.0;
+            List<Reviews> result = new List<Reviews>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            foreach (Reviews r in reviews)
+            {
+                if (r.AssociatedMovieId == movie)
+                {
+                    result.Add(r);
+                    totalMovieRating += r.Rating;
+                }
+            }
+            if (result.Count == 0)
+            {
+                throw new ArgumentException($"No reviews for reviewer with id {movie} were found");
+            }
+            else
+            {
+                avg = totalMovieRating / result.Count();
+                avg = Math.Round(avg, 2);
+            }
+            return avg;
         }
 
         public List<int> GetMostProductiveReviewers()
@@ -58,22 +79,65 @@ namespace MovieRating.Core.ApplicationService.Implementation
 
         public int GetNumberOfRates(int movie, int rate)
         {
-            throw new NotImplementedException();
+            List<Reviews> result = new List<Reviews>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            if (rate < 1 || rate > 5)
+            {
+                throw new ArgumentException("Pick a number between 1-5");
+            }
+            foreach (Reviews r in reviews)
+            {
+                if (r.AssociatedMovieId == movie && r.Rating == rate)
+                {
+                    result.Add(r);
+                }
+            }
+            return result.Count;
         }
-
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            throw new NotImplementedException();
+            List<Reviews> result = new List<Reviews>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            foreach (Reviews r in reviews)
+            {
+                if (r.ReviewerId == reviewer && r.Rating == rate)
+                {
+                    result.Add(r);
+                }
+            }
+            return result.Count;
         }
 
         public int GetNumberOfReviews(int movie)
         {
-            throw new NotImplementedException();
+            List<Reviews> result = new List<Reviews>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            foreach (Reviews r in reviews)
+            {
+                if (r.AssociatedMovieId == movie)
+                {
+                    result.Add(r);
+                }
+            }
+            return result.Count;
         }
 
         public int GetNumberOfReviewsFromReviewer(int reviewer)
         {
-            throw new NotImplementedException();
+            List<Reviews> result = new List<Reviews>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            foreach (Reviews r in reviews)
+            {
+                if (r.ReviewerId == reviewer)
+                {
+                    result.Add(r);
+                }
+            }
+            if (result.Count == 0)
+            {
+                throw new ArgumentException($"No reviews for reviewer with id {reviewer} were found");
+            }
+            return result.Count();
         }
 
         public List<int> GetReviewersByMovie(int movie)
@@ -83,7 +147,24 @@ namespace MovieRating.Core.ApplicationService.Implementation
 
         public List<int> GetTopMoviesByReviewer(int reviewer)
         {
-            throw new NotImplementedException();
+            List<int> returnList = new List<int>();
+            List<Reviews> reviews = _repo.GetAllReviews().ToList();
+            List<Reviews> sortedReviews = reviews
+              .Where(rid => rid.ReviewerId == reviewer)
+              .OrderByDescending(rating => rating.Rating)
+              .ThenByDescending(date => date.ReviewDate)
+              .ToList();
+
+            foreach (Reviews r in sortedReviews)
+            {
+                int id = r.AssociatedMovieId;
+                returnList.Add(id);
+            }
+            if (returnList.Count == 0)
+            {
+                throw new ArgumentException("there are no movies to be found");
+            }
+            return returnList;
         }
 
         public List<int> GetTopRatedMovies(int amount)
--- MovieRatingCompulsorySolution/MovieRatingMockTests/UnitTest.cs
+++ MovieRatingCompulsorySolution/MovieRatingMockTests/UnitTest.cs
@@ -81,32 +81,142 @@ namespace MovieRatingMockTests
         public void setup()
         { }
 
-        [TestMethod]
+        [TestMethod] //Question 1
         public void TestGetAverageRateFromReviewer()
         {
-            //  Setup the mock
+            // 1
             Mock<IRepository> mock = new Mock<IRepository>();
             mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
-            Service service = new Service(mock.Object);
-            //  Test Reviewer 1
-            double actualResult = service.GetAverageRateFromReviewer(1);
+            Service ReviewsService = new Service(mock.Object);
+            // 2
+            double actualResult = ReviewsService.GetAverageRateFromReviewer(1);
             mock.Verify(mock => mock.GetAllReviews());
-            Assert.IsTrue(actualResult == 2.67, "IsTrue test failed for reviewer 1");
-            Assert.IsFalse(actualResult == 22, "IsFalse test failed for reviewer 1");
-            //  Test Reviewer 2
-            double actualResult2 = service.GetAverageRateFromReviewer(2);
-            Assert.IsTrue(actualResult2 == 3.5, "IsTrue test failed for reviewer 2");
-            Assert.IsFalse(actualResult2 == 22, "IsFalse test failed for reviewer 2");
-            //  Test Reviewer 3
-            double actualResult3 = service.GetAverageRateFromReviewer(3);
-            Assert.IsTrue(actualResult3 == 2, "IsTrue test failed for reviewer 3");
-            Assert.IsFalse(actualResult3 == 22, "IsFalse test failed for reviewer 3");
-            //  Test Reviewer 4
-            double actualResult4 = service.GetAverageRateFromReviewer(4);
-            Assert.IsTrue(actualResult4 == 4.67, "IsTrue test failed for reviewer 4");
-            Assert.IsFalse(actualResult4 == 22, "IsFalse test failed for reviewer 4");
-            //  Test Exception for average from reviewer
-            Assert.ThrowsException<ArgumentException>(() => service.GetAverageRateFromReviewer(200));
+            Assert.IsTrue(actualResult == 2.67, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+
+            Assert.ThrowsException<ArgumentException>(() => ReviewsService.GetAverageRateFromReviewer(200));
+        }
+
+
+        [TestMethod] //Question 2
+        public void TestGetNumberOfReviewsFromReviewer()
+        {
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewsService = new Service(mock.Object);
+            //1
+            int actualResult = ReviewsService.GetNumberOfReviewsFromReviewer(2);
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(actualResult == 2, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+            //2
+            int actualResult2 = ReviewsService.GetNumberOfReviewsFromReviewer(1);
+            Assert.IsTrue(actualResult2 == 3, "It is the right person");
+            Assert.IsFalse(actualResult2 == 22, "It is the wrong person");
+
+            Assert.ThrowsException<ArgumentException>(() => ReviewsService.GetNumberOfReviewsFromReviewer(200));
+        }
+
+
+
+        [TestMethod] //Question 3
+        public void TestGetNumberOfRatesByReviewer()
+        {
+
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewService = new Service(mock.Object);
+            //1
+            int actualResult = ReviewService.GetNumberOfRatesByReviewer(1, 4);
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(actualResult == 1, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+            //1
+            int actualResult2 = ReviewService.GetNumberOfRatesByReviewer(2, 1);
+            Assert.IsTrue(actualResult2 == 0, "It is the right person");
+            Assert.IsFalse(actualResult2 == 22, "It is the wrong person");
+
+        }
+
+        [TestMethod] //Question 4
+        public void TestGetNumberOfReviews()
+        {
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewService = new Service(mock.Object);
+            //1
+            int actualResult = ReviewService.GetNumberOfReviews(1);
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(actualResult == 2, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+            //2
+            int actualResult2 = ReviewService.GetNumberOfReviews(2);
+            Assert.IsTrue(actualResult2 == 4, "It is the right person");
+            Assert.IsFalse(actualResult2 == 22, "It is the wrong person");
+
+            Assert.ThrowsException<ArgumentException>(() => ReviewService.GetAverageRateFromReviewer(200));
+        }
+
+        [TestMethod] //Question 5
+        public void TestGetAverageRateOfMovie()
+        {
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewService = new Service(mock.Object);
+            //1
+            double actualResult = ReviewService.GetAverageRateOfMovie(1);
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(actualResult == 4, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+            //2
+            double actualResult2 = ReviewService.GetAverageRateOfMovie(2);
+            Assert.IsTrue(actualResult2 == 2.75, "It is the right person");
+            Assert.IsFalse(actualResult2 == 22, "It is the wrong person");
+
+            Assert.ThrowsException<ArgumentException>(() => ReviewService.GetAverageRateFromReviewer(200));
+        }
+
+        [TestMethod] //Question 6
+        public void TestGetNumberOfRates()
+        {
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewService = new Service(mock.Object);
+            //1
+            int actualResult = ReviewService.GetNumberOfRates(1, 4);
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(actualResult == 2, "It is the right person");
+            Assert.IsFalse(actualResult == 22, "It is the wrong person");
+            //2
+            int actualResult2 = ReviewService.GetNumberOfRates(2, 2);
+            Assert.IsTrue(actualResult2 == 3, "It is the right person");
+            Assert.IsFalse(actualResult2 == 22, "It is the wrong person");
+
+            Assert.ThrowsException<ArgumentException>(() => ReviewService.GetAverageRateFromReviewer(200));
+        }
+
+        [TestMethod] //Question 10
+        public void TestGetTopMoviesByReviewer()
+        {
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue1);
+            Service ReviewService = new Service(mock.Object);
+
+            List<int> actualResult = ReviewService.GetTopMoviesByReviewer(1);
+            List<int> x1 = new List<int>() { 1, 3, 2 };
+            mock.Verify(mock => mock.GetAllReviews());
+            Assert.IsTrue(Enumerable.SequenceEqual(x1, actualResult), "It is the right person");
+            Assert.IsFalse(actualResult.Equals(22), "It is the wrong person");
+
+            List<int> actualResult2 = ReviewService.GetTopMoviesByReviewer(2);
+            List<int> x2 = new List<int>() { 4, 2 };
+            Assert.IsTrue(Enumerable.SequenceEqual(x2, actualResult2), "It is the right person");
+            Assert.IsFalse(actualResult2.Equals(22), "It is the wrong person");
+
+            Mock<IRepository> mock2 = new Mock<IRepository>();
+            mock2.Setup(mock => mock.GetAllReviews()).Returns(() => returnValue2);
+            Service ReviewService2 = new Service(mock2.Object);
+            Assert.ThrowsException<ArgumentException>(() => ReviewService2.GetTopMoviesByReviewer(1));
         }
 
 

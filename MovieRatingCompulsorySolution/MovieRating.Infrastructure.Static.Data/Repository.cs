1edf143ba2d8aa614bb29100ea6f6c96a993481c
abdi2148 <abdiqafar.abas@gmail.com>
2020-10-04 15:42:39 +0200

    UnitTest

--- MovieRatingCompulsorySolution/MovieRating.Infrastructure.Static.Data/Repository.cs
+++ MovieRatingCompulsorySolution/MovieRating.Infrastructure.Static.Data/Repository.cs
@@ -2,10 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MovieRating.Core.DomainService;
+using MovieRating.Core.Entity;
 
 namespace MovieRating.Infrastructure.Static.Data
 {
-    public class Repository: IRepository
+
+    public class Repository : IRepository
     {
-    }
+        List<Reviews> reviews;
+
+        public Repository()
+        {
+            reviews = new List<Reviews>();
+        }
+
+
+        public IEnumerable<Reviews> GetAllReviews()
+        {
+            return new List<Reviews>(reviews);
+        }
+    };
+
 }
---
+++ MovieRatingCompulsorySolution/MovieRatingMockTests/UnitTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MovieRating.Core.ApplicationService.Services;
+using MovieRating.Core.DomainService;
+using MovieRating.Core.Entity;
+
+namespace MovieRatingMockTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+
+        [TestMethod]
+        public void TestGetAverageRateFromReviewer()
+        {
+            Mock<IRepository> mt = new Mock<IRepository>();
+
+            Reviews[] returnValue =
+              { new Reviews  {Reviewer = 1, Movie = 2, Grade = 3 },
+                new Reviews {Reviewer = 1, Movie = 2, Grade = 4 },
+                new Reviews {Reviewer = 1, Movie = 3, Grade = 2 },
+                new Reviews {Reviewer = 3, Movie = 3, Grade = 2 }
+            };
+
+            mt.Setup(mt => mt.GetAllReviews()).Returns(() => value);
+
+            ReviewService reviewService = new ReviewService(mt.Object);
+
+            double aResult = reviewService.GetAverageRate(1);
+
+            mt.Verify(mt => mt.GetAllRatings(), Times.Once);
+
+            Assert.IsTrue(actualResult.Count == 1);
+            Assert.IsTrue(actualResult.Contains(returnValue[1]));
+        }
+
+
+    }
+
+
+    public void TestGetNumberOfReviewsFromReviewer()
+    {
+        Mock<IRepository> mt = new Mock<IRepository>();
+
+
+        Reviews[] returnValue =
+            { new Review  {Reviewer = 1, Movie = 2, Grade = 3 },
+                new Review {Reviewer = 2, Movie = 2, Grade = 4 } };
+
+        mt.Setup(mt => mt.GetAllReviews()).Returns(() => value);
+
+        ReviewService reviewService = new ReviewService(mt.Object);
+
+        int aResult = reviewService.GetNumberOfReviewsFromReviewer(2);
+
+        mt.Verify(mt => mt.GetAllReviews(), Times.Once);
+
+        Assert.IsTrue(actualResult.Count == 1);
+        Assert.IsTrue(actualResult.Contains(returnValue[1]));
+
+    }
+
+
+
+}
